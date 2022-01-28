7d431f8d9696016c64205e69980f739f7127514b
unknown <barnes.kourt@gmail.com>
2022-01-28 11:23:45 -0500

    push

---
+++ app.js
@@ -0,0 +1,91 @@
+//1. Create a function that returns the sum of two arguments
+function add(a,b) {
+ return a + b;
+}
+console.log(add(1,2))
+
+//2. and 3. Create a function that returns the sum of two arguments
+function add(a,b) {
+    return a + b;
+   }
+   console.log(add(1,2))
+   
+   //4. function that adds three arguments together
+   function addThreeFunctions(a,b,c) {
+       return a + b + c;
+   }
+   console.log(addThreeFunctions(1,2,3))
+   
+   //5. function that adds four arguments together
+   function addFourFunctions(a,b,c,d) {
+       return a + b + c + d;
+   }
+   console.log(addFourFunctions(1,2,3,4))
+   
+   //6.
+   var mySandwich = {
+       "bread": 2,
+       "meat": "turkey",
+       "veggies": "onions",
+   };
+   
+   //7.
+   var breadValue = mySandwich.bread;
+   
+   //8.
+   var meatType = mySandwich[meat];
+   
+   //9.
+   mySandwich.sauce = "mayo";
+   
+   //10.
+   mySandwich.meat = "roast beef";
+   
+   
+   
+   
+   
+   
+   //6.
+   var myCar = {
+       "wheels": 4,
+       "doors": 2,
+       "color": "pink",
+   };
+   
+   //7.
+   var wheelValue = myCar.wheels;
+   
+   //8.
+   var carColor = myCar["color"];
+   
+   //9.
+   myCar.type = "Kia";
+   
+   //10.
+   myCar.color = "red";
+   
+   
+   
+   
+   
+   
+   
+   //6.
+   var myRoom = {
+       "doors": 1,
+       "flooring": "hardwood",
+       "windows": 3,
+   };
+   
+   //7.
+   var windowValue = myRoom.windows;
+   
+   //8.
+   var floorType = myRoom["flooring"];
+   
+   //9.
+   myRoom.beds = 1;
+   
+   //10.
+   myRoom.flooring = "carpet";
